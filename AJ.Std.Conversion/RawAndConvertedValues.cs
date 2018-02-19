54ddf767e2f0682287d52e7ab9375ad8a85af42e
Alexey Debelov <aj001@mail.ru>
2018-02-19 15:02:30 +0500

    Added .Conversion project

---
+++ AJ.Std.Conversion/RawAndConvertedValues.cs
@@ -0,0 +1,13 @@
+﻿using AJ.Std.Conversion.Contracts;
+
+namespace AJ.Std.Conversion {
+	public sealed class RawAndConvertedValues<TRaw, TConverted> {
+		private readonly IBuilderOneToOne<TRaw, TConverted> _builder;
+		public TRaw RawValue { get; }
+		public RawAndConvertedValues(TRaw rawValue, IBuilderOneToOne<TRaw, TConverted> builder) {
+			_builder = builder;
+			RawValue = rawValue;
+		}
+		public TConverted ConvertedValue => _builder.Build(RawValue);
+	}
+}
