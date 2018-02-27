7a65090b34d74e0612d84e851ea3e4faa3cd862f
Alexey Debelov <aj001@mail.ru>
2018-02-27 16:08:59 +0500

    Fixes in RawAndConvertedValuesWithStringPresentation class

--- AJ.Std.Conversion/RawAndConvertedValuesWithStringPresentation.cs
+++ AJ.Std.Conversion/RawAndConvertedValuesWithStringPresentation.cs
@@ -1,19 +1,22 @@
 ﻿using AJ.Std.Conversion.Contracts;
 
-namespace AJ.Std.Conversion
-{
+namespace AJ.Std.Conversion {
 	public sealed class RawAndConvertedValuesWithStringPresentation<TRaw, TConverted> : IRawAndConvertedValues<TRaw, TConverted> {
+		private readonly IBuilderOneToOne<TRaw, string> _rawStringBuilder;
 		private readonly IBuilderOneToOne<TRaw, TConverted> _builder;
+		private readonly IBuilderOneToOne<TConverted, string> _convertStringBuilder;
 		public TRaw RawValue { get; }
 
-		public RawAndConvertedValuesWithStringPresentation(TRaw rawValue, IBuilderOneToOne<string, TRaw> rawStringBuilder, IBuilderOneToOne<TRaw, TConverted> builder, IBuilderOneToOne<string, TRaw> convertStringBuilder) {
-			_builder = builder;
+		public RawAndConvertedValuesWithStringPresentation(TRaw rawValue, IBuilderOneToOne<TRaw, string> rawStringBuilder, IBuilderOneToOne<TRaw, TConverted> builder, IBuilderOneToOne<TConverted, string> convertStringBuilder) {
 			RawValue = rawValue;
+			_rawStringBuilder = rawStringBuilder;
+			_builder = builder;
+			_convertStringBuilder = convertStringBuilder;
 		}
 		public TConverted ConvertedValue => _builder.Build(RawValue);
 
 		public override string ToString() {
-			return RawValue + " - " + ConvertedValue;
+			return (_rawStringBuilder != null ? _rawStringBuilder.Build(RawValue) : RawValue.ToString()) + " - " + (_convertStringBuilder != null ? _convertStringBuilder.Build(ConvertedValue) : ConvertedValue.ToString());
 		}
 	}
 }
