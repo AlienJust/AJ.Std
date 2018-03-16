da9a3fd028ddc0b2bd3e3d15aa21816982f032a3
Alexey Debelov <aj001@mail.ru>
2018-03-16 14:25:35 +0500

    Added simplest IRawAndConvertedValues implementation

--- AJ.Std.Conversion/BuilderOneToOneNullableString.cs
+++ AJ.Std.Conversion/BuilderOneToOneNullableString.cs
@@ -1,20 +1,16 @@
 ﻿using AJ.Std.Conversion.Contracts;
 
-namespace AJ.Std.Conversion
-{
-	public sealed class BuilderOneToOneNullableString<TRaw> : IBuilderOneToOne<TRaw?, string> where TRaw: struct
-	{
+namespace AJ.Std.Conversion {
+	public sealed class BuilderOneToOneNullableString<TRaw> : IBuilderOneToOne<TRaw?, string> where TRaw : struct {
 		private readonly IBuilderOneToOne<TRaw, string> _builder;
 		private readonly string _nullValue;
 
-		public BuilderOneToOneNullableString(IBuilderOneToOne<TRaw, string> builder, string nullValue)
-		{
+		public BuilderOneToOneNullableString(IBuilderOneToOne<TRaw, string> builder, string nullValue) {
 			_builder = builder;
 			_nullValue = nullValue;
 		}
 
-		public string Build(TRaw? source)
-		{
+		public string Build(TRaw? source) {
 			if (!source.HasValue) return _nullValue;
 			return _builder.Build(source.Value);
 		}
---
+++ AJ.Std.Conversion/RawAndConvertedValuesSimple.cs
@@ -0,0 +1,18 @@
+﻿using AJ.Std.Conversion.Contracts;
+
+namespace AJ.Std.Conversion
+{
+	public sealed class RawAndConvertedValuesSimple<TRaw, TConverted> : IRawAndConvertedValues<TRaw, TConverted> {
+		public TRaw RawValue { get; }
+		public TConverted ConvertedValue { get; }
+
+		public RawAndConvertedValuesSimple(TRaw rawValue, TConverted convertedValue) {
+			RawValue = rawValue;
+			ConvertedValue = convertedValue;
+		}
+
+		public override string ToString() {
+			return RawValue + " - " + ConvertedValue;
+		}
+	}
+}
