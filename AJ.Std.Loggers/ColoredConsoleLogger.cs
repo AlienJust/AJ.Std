7fe9cf916aaf8a8a1e2c9b6b732dd2f180da1ff3
Alexey Debelov <aj001@mail.ru>
2018-06-20 15:24:16 +0500

    Added not versioned files

---
+++ AJ.Std.Loggers/ColoredConsoleLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AJ.Std.Loggers.Contracts;
+
+namespace AJ.Std.Loggers
+{
+	public class ColoredConsoleLogger : ILogger {
+		/// <summary>
+		/// sync object for all loggers
+		/// </summary>
+		private static readonly object Sync = new object();
+		
+		private readonly ConsoleColor _forecolor;
+		private readonly ConsoleColor _backcolor;
+
+		public ColoredConsoleLogger(ConsoleColor forecolor, ConsoleColor backcolor) {
+			_forecolor = forecolor;
+			_backcolor = backcolor;
+		}
+
+		public void Log(string text) {
+			lock (Sync) {
+				var currentConsoleColor = Console.ForegroundColor;
+				var currentConsoleBackColor = Console.BackgroundColor;
+				Console.ForegroundColor = _forecolor;
+				Console.BackgroundColor = _backcolor;
+				Console.WriteLine(text);
+				Console.ForegroundColor = currentConsoleColor;
+				Console.BackgroundColor = currentConsoleBackColor;
+			}
+		}
+
+		public void Log(object obj) {
+			Log(obj.ToString());
+		}
+	}
+}
