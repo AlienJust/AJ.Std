3f53fc0b2a7811cc7cee8d25157d380ea4f2e700
Alexey Debelov <alienjustmail@gmail.com>
2019-11-20 22:14:15 +0500

    Building nugets Added LICENSE.txt

--- AlienJust.Support.Identy.Contracts/IStorage.cs
+++ AlienJust.Support.Identy.Contracts/IStorage.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 
-namespace AlienJust.Support.Identy.Contracts {
-	// move to project AlienJust.Support.Store when growing
-	public interface IStorage<TDataItem> where TDataItem : IObjectWithIdentifier {
-		IEnumerable<TDataItem> StoredItems { get; }
-		void Add(TDataItem item);
-		void Remove(TDataItem item);
-		void Update(IIdentifier id, TDataItem item);
-	}
+namespace AlienJust.Support.Identy.Contracts
+{
+    // move to project AlienJust.Support.Store when growing
+    public interface IStorage<TDataItem> where TDataItem : IObjectWithIdentifier
+    {
+        IEnumerable<TDataItem> StoredItems { get; }
+        void Add(TDataItem item);
+        void Remove(TDataItem item);
+        void Update(IIdentifier id, TDataItem item);
+    }
 }
