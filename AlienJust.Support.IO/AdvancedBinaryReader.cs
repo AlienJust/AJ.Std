f06d242b194f9a9c61c8eafdadcc587646cdd024
Alexey Debelov <alienjustmail@gmail.com>
2019-11-22 23:52:20 +0500

    IO packaged!

--- AlienJust.Support.IO/AdvancedBinaryReader.cs
+++ AlienJust.Support.IO/AdvancedBinaryReader.cs
@@ -1,28 +1,32 @@
 ﻿using System.IO;
 using System.Text;
 
-namespace AlienJust.Support.IO {
-	/// <summary>
-	/// Allows to use big endian bytes order when reading from stream, based on System.IO.BinaryReader
-	/// </summary>
-	public class AdvancedBinaryReader : BinaryReader {
-		private bool _isLittleEndian;
-		private readonly byte[] _buffer = new byte[8];
+namespace AlienJust.Support.IO
+{
+    /// <summary>
+    /// Allows to use big endian bytes order when reading from stream, based on System.IO.BinaryReader
+    /// </summary>
+    public class AdvancedBinaryReader : BinaryReader
+    {
+        private bool _isLittleEndian;
+        private readonly byte[] _buffer = new byte[8];
 
-		public AdvancedBinaryReader(Stream input, Encoding encoding, bool isLittleEndian)
-			: base(input, encoding) {
-			_isLittleEndian = isLittleEndian;
-		}
+        public AdvancedBinaryReader(Stream input, Encoding encoding, bool isLittleEndian)
+            : base(input, encoding)
+        {
+            _isLittleEndian = isLittleEndian;
+        }
 
-		public AdvancedBinaryReader(Stream input, bool isLittleEndian)
-			: this(input, Encoding.UTF8, isLittleEndian) { }
+        public AdvancedBinaryReader(Stream input, bool isLittleEndian)
+            : this(input, Encoding.UTF8, isLittleEndian) { }
 
-		public bool IsLittleEndian {
-			get => _isLittleEndian;
-			set => _isLittleEndian = value;
-		}
+        public bool IsLittleEndian
+        {
+            get => _isLittleEndian;
+            set => _isLittleEndian = value;
+        }
 
-		/*
+        /*
         public override unsafe double ReadDouble()
         {
             if (isLittleEndian)
@@ -35,30 +39,33 @@ namespace AlienJust.Support.IO {
         }
         */
 
-		public override short ReadInt16() {
-			if (_isLittleEndian)
-				return base.ReadInt16();
-			FillMyBuffer(2);
-			return (short)(_buffer[1] | (_buffer[0] << 8));
-		}
+        public override short ReadInt16()
+        {
+            if (_isLittleEndian)
+                return base.ReadInt16();
+            FillMyBuffer(2);
+            return (short)(_buffer[1] | (_buffer[0] << 8));
+        }
 
-		public override int ReadInt32() {
-			if (_isLittleEndian)
-				return base.ReadInt32();
-			FillMyBuffer(4);
-			return _buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18);
-		}
+        public override int ReadInt32()
+        {
+            if (_isLittleEndian)
+                return base.ReadInt32();
+            FillMyBuffer(4);
+            return _buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18);
+        }
 
-		public override long ReadInt64() {
-			if (_isLittleEndian)
-				return base.ReadInt64();
-			FillMyBuffer(8);
-			var num = (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
-			var num2 = (uint)(_buffer[7] | (_buffer[6] << 8) | (_buffer[5] << 0x10) | (_buffer[4] << 0x18));
-			return (num2 << 0x20) | num;
-		}
+        public override long ReadInt64()
+        {
+            if (_isLittleEndian)
+                return base.ReadInt64();
+            FillMyBuffer(8);
+            var num = (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
+            var num2 = (uint)(_buffer[7] | (_buffer[6] << 8) | (_buffer[5] << 0x10) | (_buffer[4] << 0x18));
+            return (num2 << 0x20) | num;
+        }
 
-		/*
+        /*
         public override unsafe float ReadSingle()
         {
             if (isLittleEndian)
@@ -68,51 +75,60 @@ namespace AlienJust.Support.IO {
             return *(((float*)&num));
         }
         */
-		//[CLSCompliant(false)]
-		public override ushort ReadUInt16() {
-			if (_isLittleEndian)
-				return base.ReadUInt16();
-			FillMyBuffer(2);
-			return (ushort)(_buffer[1] | (_buffer[0] << 8));
-		}
+        //[CLSCompliant(false)]
+        public override ushort ReadUInt16()
+        {
+            if (_isLittleEndian)
+                return base.ReadUInt16();
+            FillMyBuffer(2);
+            return (ushort)(_buffer[1] | (_buffer[0] << 8));
+        }
 
-		//[CLSCompliant(false)]
-		public override uint ReadUInt32() {
-			if (_isLittleEndian)
-				return base.ReadUInt32();
-			FillMyBuffer(4);
-			return (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
-		}
+        //[CLSCompliant(false)]
+        public override uint ReadUInt32()
+        {
+            if (_isLittleEndian)
+                return base.ReadUInt32();
+            FillMyBuffer(4);
+            return (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
+        }
 
-		//[CLSCompliant(false)]
-		public override ulong ReadUInt64() {
-			if (_isLittleEndian)
-				return base.ReadUInt64();
-			FillMyBuffer(8);
-			var num = (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
-			var num2 = (uint)(_buffer[7] | (_buffer[6] << 8) | (_buffer[5] << 0x10) | (_buffer[4] << 0x18));
-			return ((num2 << 0x20) | num);
-		}
+        //[CLSCompliant(false)]
+        public override ulong ReadUInt64()
+        {
+            if (_isLittleEndian)
+                return base.ReadUInt64();
+            FillMyBuffer(8);
+            var num = (uint)(_buffer[3] | (_buffer[2] << 8) | (_buffer[1] << 0x10) | (_buffer[0] << 0x18));
+            var num2 = (uint)(_buffer[7] | (_buffer[6] << 8) | (_buffer[5] << 0x10) | (_buffer[4] << 0x18));
+            return ((num2 << 0x20) | num);
+        }
 
-		private void FillMyBuffer(int numBytes) {
-			int offset = 0;
-			int num2 = 0;
-			if (numBytes == 1) {
-				num2 = BaseStream.ReadByte();
-				if (num2 == -1) {
-					throw new EndOfStreamException("Attempted to read past the end of the stream.");
-				}
-				_buffer[0] = (byte)num2;
-			}
-			else {
-				do {
-					num2 = BaseStream.Read(_buffer, offset, numBytes - offset);
-					if (num2 == 0) {
-						throw new EndOfStreamException("Attempted to read past the end of the stream.");
-					}
-					offset += num2;
-				} while (offset < numBytes);
-			}
-		}
-	}
+        private void FillMyBuffer(int numBytes)
+        {
+            int offset = 0;
+            int num2 = 0;
+            if (numBytes == 1)
+            {
+                num2 = BaseStream.ReadByte();
+                if (num2 == -1)
+                {
+                    throw new EndOfStreamException("Attempted to read past the end of the stream.");
+                }
+                _buffer[0] = (byte)num2;
+            }
+            else
+            {
+                do
+                {
+                    num2 = BaseStream.Read(_buffer, offset, numBytes - offset);
+                    if (num2 == 0)
+                    {
+                        throw new EndOfStreamException("Attempted to read past the end of the stream.");
+                    }
+                    offset += num2;
+                } while (offset < numBytes);
+            }
+        }
+    }
 }
--- AlienJust.Support.IO/AdvancedBinaryWriter.cs
+++ AlienJust.Support.IO/AdvancedBinaryWriter.cs
@@ -3,24 +3,27 @@ using System.Text;
 
 namespace AlienJust.Support.IO
 {
-	public class AdvancedBinaryWriter : BinaryWriter {
-		private bool _isLittleEndian;
-		private readonly byte[] _buffer = new byte[8];
+    public class AdvancedBinaryWriter : BinaryWriter
+    {
+        private bool _isLittleEndian;
+        private readonly byte[] _buffer = new byte[8];
 
-		public AdvancedBinaryWriter(Stream output, Encoding encoding, bool isLittleEndian)
-			: base(output, encoding) {
-			_isLittleEndian = isLittleEndian;
-		}
+        public AdvancedBinaryWriter(Stream output, Encoding encoding, bool isLittleEndian)
+            : base(output, encoding)
+        {
+            _isLittleEndian = isLittleEndian;
+        }
 
-		public AdvancedBinaryWriter(Stream output, bool isLittleEndian)
-			: this(output, Encoding.UTF8, isLittleEndian) { }
+        public AdvancedBinaryWriter(Stream output, bool isLittleEndian)
+            : this(output, Encoding.UTF8, isLittleEndian) { }
 
-		public bool IsLittleEndian {
-			get => _isLittleEndian;
-			set => _isLittleEndian = value;
-		}
+        public bool IsLittleEndian
+        {
+            get => _isLittleEndian;
+            set => _isLittleEndian = value;
+        }
 
-		/*
+        /*
         public override unsafe void Write(double value)
         {
             if (isLittleEndian)
@@ -42,45 +45,51 @@ namespace AlienJust.Support.IO
         }
         */
 
-		public override void Write(short value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[1] = (byte)value;
-			_buffer[0] = (byte)(value >> 8);
-			OutStream.Write(_buffer, 0, 2);
-		}
+        public override void Write(short value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[1] = (byte)value;
+            _buffer[0] = (byte)(value >> 8);
+            OutStream.Write(_buffer, 0, 2);
+        }
 
-		public override void Write(int value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[3] = (byte)value;
-			_buffer[2] = (byte)(value >> 8);
-			_buffer[1] = (byte)(value >> 0x10);
-			_buffer[0] = (byte)(value >> 0x18);
-			OutStream.Write(_buffer, 0, 4);
-		}
+        public override void Write(int value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[3] = (byte)value;
+            _buffer[2] = (byte)(value >> 8);
+            _buffer[1] = (byte)(value >> 0x10);
+            _buffer[0] = (byte)(value >> 0x18);
+            OutStream.Write(_buffer, 0, 4);
+        }
 
-		public override void Write(long value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[7] = (byte)value;
-			_buffer[6] = (byte)(value >> 8);
-			_buffer[5] = (byte)(value >> 0x10);
-			_buffer[4] = (byte)(value >> 0x18);
-			_buffer[3] = (byte)(value >> 0x20);
-			_buffer[2] = (byte)(value >> 40);
-			_buffer[1] = (byte)(value >> 0x30);
-			_buffer[0] = (byte)(value >> 0x38);
-			OutStream.Write(_buffer, 0, 8);
-		}
+        public override void Write(long value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[7] = (byte)value;
+            _buffer[6] = (byte)(value >> 8);
+            _buffer[5] = (byte)(value >> 0x10);
+            _buffer[4] = (byte)(value >> 0x18);
+            _buffer[3] = (byte)(value >> 0x20);
+            _buffer[2] = (byte)(value >> 40);
+            _buffer[1] = (byte)(value >> 0x30);
+            _buffer[0] = (byte)(value >> 0x38);
+            OutStream.Write(_buffer, 0, 8);
+        }
 
-		/*
+        /*
         public override unsafe void Write(float value)
         {
             if (isLittleEndian)
@@ -96,45 +105,51 @@ namespace AlienJust.Support.IO
              OutStream.Write(buffer, 0, 4);
         }
         */
-		//[CLSCompliant(false)]
-		public override void Write(ushort value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[1] = (byte)value;
-			_buffer[0] = (byte)(value >> 8);
-			OutStream.Write(_buffer, 0, 2);
-		}
+        //[CLSCompliant(false)]
+        public override void Write(ushort value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[1] = (byte)value;
+            _buffer[0] = (byte)(value >> 8);
+            OutStream.Write(_buffer, 0, 2);
+        }
 
-		//[CLSCompliant(false)]
-		public override void Write(uint value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[3] = (byte)value;
-			_buffer[2] = (byte)(value >> 8);
-			_buffer[1] = (byte)(value >> 0x10);
-			_buffer[0] = (byte)(value >> 0x18);
-			OutStream.Write(_buffer, 0, 4);
-		}
+        //[CLSCompliant(false)]
+        public override void Write(uint value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[3] = (byte)value;
+            _buffer[2] = (byte)(value >> 8);
+            _buffer[1] = (byte)(value >> 0x10);
+            _buffer[0] = (byte)(value >> 0x18);
+            OutStream.Write(_buffer, 0, 4);
+        }
 
-		//[CLSCompliant(false)]
-		public override void Write(ulong value) {
-			if (_isLittleEndian) {
-				base.Write(value);
-				return;
-			}
-			_buffer[7] = (byte)value;
-			_buffer[6] = (byte)(value >> 8);
-			_buffer[5] = (byte)(value >> 0x10);
-			_buffer[4] = (byte)(value >> 0x18);
-			_buffer[3] = (byte)(value >> 0x20);
-			_buffer[2] = (byte)(value >> 40);
-			_buffer[1] = (byte)(value >> 0x30);
-			_buffer[0] = (byte)(value >> 0x38);
-			OutStream.Write(_buffer, 0, 8);
-		}
-	}
+        //[CLSCompliant(false)]
+        public override void Write(ulong value)
+        {
+            if (_isLittleEndian)
+            {
+                base.Write(value);
+                return;
+            }
+            _buffer[7] = (byte)value;
+            _buffer[6] = (byte)(value >> 8);
+            _buffer[5] = (byte)(value >> 0x10);
+            _buffer[4] = (byte)(value >> 0x18);
+            _buffer[3] = (byte)(value >> 0x20);
+            _buffer[2] = (byte)(value >> 40);
+            _buffer[1] = (byte)(value >> 0x30);
+            _buffer[0] = (byte)(value >> 0x38);
+            OutStream.Write(_buffer, 0, 8);
+        }
+    }
 }
